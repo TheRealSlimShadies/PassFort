a01f217d9a3027f3f5319b9a91b9733a4881b39e
Samir <imsamirthepro@gmail.com>
2024-12-28 13:44:58 +0545

    made a little change to the models and credential retrievel where you use name instead of id

--- Backend/Passfort/vault/urls.py
+++ Backend/Passfort/vault/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('labels/<int:label_id>/delete/', views.delete_vault_label, name='delete_vault_label'), 
 
     # User Credential endpoints
-    path('labels/<int:label_id>/credentials/', views.get_user_credentials, name='get_user_credentials'), 
-    path('labels/<int:label_id>/credentials/create/', views.create_user_credential, name='create_user_credential'),  
-    path('labels/<int:label_id>/credentials/<int:credential_id>/delete/', views.delete_user_credential, name='delete_user_credential'),  
+    path('labels/<str:label_name>/credentials/', views.get_user_credentials, name='get_user_credentials'), 
+    path('labels/<str:label_name>/credentials/create/', views.create_user_credential, name='create_user_credential'),  
+    path('labels/<str:label_name>/credentials/<int:credential_id>/delete/', views.delete_user_credential, name='delete_user_credential'),  
 ]
