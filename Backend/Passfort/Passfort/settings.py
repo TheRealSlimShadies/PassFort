d2ad2ed21fec7de46efd13986de03b80d03fa55f
Samir <imsamirthepro@gmail.com>
2025-01-05 20:28:39 +0545

    added update data views

--- Backend/Passfort/Passfort/settings.py
+++ Backend/Passfort/Passfort/settings.py
@@ -176,7 +176,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
